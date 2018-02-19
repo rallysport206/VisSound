67b9a39ab4f2dca99d8fbec83d72a6b1edc4a1ab
rallysport206 <phan.tony125@gmail.com>
2018-02-18 17:04:42 -0800

    sound file and scripts

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var song;
 
+function preload() {
+  song = loadSound("rtj.mp3"); 
+}
+
 function setup() {
-  createCanvas(200,200);
-  song = load
+  createCanvas(200, 200);
+  song.play();
+}
+
+function draw() {
+  background(0);
 }
