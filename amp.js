d01ea49009239bebe2360269b2a41b03cc1b029c
rallysport206 <phan.tony125@gmail.com>
2018-02-20 13:18:33 -0800

    added different graphs

---
+++ amp.js
@@ -0,0 +1,37 @@
+var song;
+var button;
+var amp;
+
+function setup() {
+  createCanvas(500, 500);
+  song = loadSound("dna.mp3", loaded);
+  amp = new p5.Amplitude();
+  background(51);
+}
+
+function loaded() {
+  button = createButton("play");
+  button.mousePressed(togglePlaying);
+}
+
+function draw() {
+  background(51);
+
+  var vol = amp.getLevel();
+  var diam = map(vol, 0, 0.3, 10, 200);
+
+  fill(255, 255, 0);
+  ellipse(width / 2, height / 2, diam, diam);
+}
+
+
+function togglePlaying() {
+  if (!song.isPlaying()) {
+    song.play();
+    song.setVolume(0.3);
+    button.html("pause");
+  } else {
+    song.stop();
+    button.html("play");
+  }
+}
--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var song;
-
-function preload() {
-  song = loadSound("rtj.mp3"); 
-}
+var sliderRate;
+var sliderPan;
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(500, 500);
+  song = loadSound("rtj.mp3", loaded); // sound being loaded
+  // button = createButton("play/pause")
+  song.setVolume(0.5);
+  sliderRate = createSlider(0, 1.5, 1, 0.01);
+  sliderPan = createSlider(-1, 1, 0, 0.01);
+}
+
+function loaded() {
   song.play();
 }
 
 function draw() {
-  background(0);
-}
+  background(random(255));
+  song.pan(sliderPan.value());
+  song.rate(sliderRate.value());
+ }
